89075fb14282fc025e4e5eda6df7dc9405351972
Cheney_Ni <ncj19991213@126.com>
2022-04-21 02:10:39 +0200

    fix lint

--- src/pages/ProblemSetDetail/BeforeAvailable/index.tsx
+++ src/pages/ProblemSetDetail/BeforeAvailable/index.tsx
@@ -1,10 +1,10 @@
+import { Col, Row, Typography } from 'antd'
 import type React from 'react'
-import { Row, Col, Typography } from 'antd'
 
 const Index: React.FC = () => (
-	<Row justify="center" align='middle' style={{ marginTop: 200 }}>
+	<Row justify='center' align='middle' style={{ marginTop: 200 }}>
 		<Col>
-			<Typography.Title>ProblemSet hasn't started yet!</Typography.Title>
+			<Typography.Title>ProblemSet hasn&apos;t started yet!</Typography.Title>
 		</Col>
 	</Row>
 )
